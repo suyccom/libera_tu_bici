f7f0853b2efb52417bb36f56532da0876205941f
Ignacio Huerta <ignacio@ihuerta.net>
2013-04-10 18:18:03 +0200

    Mostrar en el mapa también las bicis no disponibles con otro color

--- public/javascripts/openlayers_libera.js
+++ public/javascripts/openlayers_libera.js
@@ -7,7 +7,7 @@ function openlayers_map() {
     map.addLayer(layer);
     
     // Centramos el mapa en Vitoria
-    find_address('Vitoria', center_map, 'none');
+    find_address('Vitoria', center_map, 'none', 'none');
     
     // Añadimos una capa de Markers
     markers = new OpenLayers.Layer.Markers( "Markers" );
@@ -22,13 +22,13 @@ function openlayers_map() {
 }
 
 
-function find_address(address, callback, url) {
+function find_address(address, callback, marker_color, url) {
   OpenLayers.Request.GET({
     url: "http://nominatim.openstreetmap.org/search?q="+address+"&format=json&limit=1",
     scope: this,
     failure: this.requestFailure,
     success: function(response) {
-       callback(response, url);
+       callback(response, marker_color, url);
     },
     headers: {"Content-Type": "application/x-www-form-urlencoded"}
   });
@@ -40,7 +40,7 @@ function requestFailure(response) {
 }
 
 
-function center_map(response, url) {
+function center_map(response, marker_color, url) {
   result = JSON.parse(response.responseText)
   var foundPosition = new OpenLayers.LonLat(result[0].lon, result[0].lat).transform(
           new OpenLayers.Projection("EPSG:4326"),
@@ -50,7 +50,7 @@ function center_map(response, url) {
 }
 
 
-function add_marker(response, url) {
+function add_marker(response, marker_color, url) {
   // Procesamos la respuesta
   result = JSON.parse(response.responseText)
   var foundPosition = new OpenLayers.LonLat(result[0].lon, result[0].lat).transform(
@@ -61,7 +61,7 @@ function add_marker(response, url) {
   // Creamos el icono
   var size = new OpenLayers.Size(21,25);
   var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-  var icon = new OpenLayers.Icon('/javascripts/img/marker-green.png', size, offset);
+  var icon = new OpenLayers.Icon('/javascripts/img/marker-'+ marker_color +'.png', size, offset);
   marker = new OpenLayers.Marker(foundPosition,icon);
   
   // Al hacer clic, ir a la URL
